29cc572a904712eb1c4b0423eddde7900bd4656f
DL1719 <32252099+DL1719@users.noreply.github.com>
2017-10-18 03:51:31 -0500

    Inicio
    Crear archivo para realizar practicas de C

---
+++ inicio.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hola baby, aqui iniciaremos con las practicas de progra, buscaré ejercicios o los que te dejen y los hacemos juntos, mañana te explico como utilizar github\n");
+	printf("Te Amo\n");
+}
